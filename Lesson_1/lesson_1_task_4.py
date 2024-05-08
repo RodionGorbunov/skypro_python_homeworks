ba52486762e86a22e62dcd20ecdf8a9f19bf7c20
Rodion.Gorbunov <rodion.gorbunov11@gmail.com>
2024-05-08 20:54:43 +0400

    HW5

--- Lesson_1/lesson_1_task_4.py
+++ Lesson_1/lesson_1_task_4.py
@@ -1,9 +1,4 @@
 first_name = input('Введите ваше имя: ')
 last_name = input('Введите вашу фамилию: ')
 
-
-<<<<<<< HEAD:Lesson_1/lesson_1_task_4.py
-print('Вас зовут:', first_name + last_name)
-=======
-print('Вас зовут:', first_name +last_name)
->>>>>>> 1ec0ff5375886b49ce743259042758043df60aa9:Lesson1/lesson_1_task_4.py
+print('Вас зовут:', last_name,  first_name)
--- Lesson_5/task_3_5_Chrome.py
+++ Lesson_5/task_3_5_Chrome.py
@@ -1,8 +1,7 @@
-# открываем страницу в браузере Chrome
+ # открываем страницу в браузере Chrome
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 
 driver = webdriver.Chrome()
 
